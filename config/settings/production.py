edee6df8579145cdf7a68d6aaf99adace0cbad88
SuperSuperStore <steven@supersuperstore.com>
2022-02-11 16:54:56 +0000

    Updated allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,9 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["novnoviumvideo.jcloud.ik-server.com"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS", default=["novnoviumvideo.jcloud.ik-server.com"]
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
